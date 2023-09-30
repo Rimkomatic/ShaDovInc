e67098cbd08aa12ac0dc0473d7c97e8cb804561a
W41K3R0 <mahapatrarik123@gmail.com>
2023-10-01 05:29:27 +0530

    added the text effects

---
+++ helpers/scripts/app.js
@@ -0,0 +1,28 @@
+gsap.registerPlugin(ScrollTrigger);
+
+
+// ! Text ----------------------------------------
+gsap.from('#logo',{
+    opacity:0,
+    x:100,
+    scrollTrigger:{
+      trigger:'#logo',
+      scrub:true,
+      markers:true,
+      start: "top center" ,
+      end:"top end"
+    }
+})
+  
+gsap.fromTo('.text-eff',{opacity:0.1},{
+    opacity:1,
+    stagger:1.5,
+    duration:2,
+    scrollTrigger:{
+      trigger:'.reveal-txt',
+      scrub:true,
+      markers:true,
+      start:'top bottom -20%',
+      end:'top center'
+    }
+})
