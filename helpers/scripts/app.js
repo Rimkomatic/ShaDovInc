d2c1211d75745de3967f4493fd3089daabedb16c
W41K3R0 <mahapatrarik123@gmail.com>
2023-10-03 22:51:15 +0530

    final frontend

--- helpers/scripts/app.js
+++ helpers/scripts/app.js
@@ -36,7 +36,24 @@ gsap.from(
     scrollTrigger:{
       trigger:'.form-holder',
       scrub:true,
-      markers:true,
+      // markers:true,
+      start:'top 70%',
+      end:'top 50%'
+    }
+  }
+);
+gsap.fromTo(
+  ".show",
+  {opacity:0 , y: 100},
+  { 
+    opacity:1, 
+    y:0,
+    ease: "power4.out",
+    stagger: 0.15,
+    scrollTrigger:{
+      trigger:'.form-holder',
+      scrub:true,
+      // markers:true,
       start:'top 70%',
       end:'top 50%'
     }
